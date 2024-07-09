26c1dd6ef4a7ed32b2875cfc256f7a29f34dd720
eddie Hinojosa <eddie_hinojosa@live.com>
2024-07-08 21:26:52 -0400

    removed routes folder, condensing to server.js file, added preliminary GET for notes

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
-// Set requirements for server.js
-const express = require("express");
-const routes = require("./routes");
+const express = require('express');
 const app = express();
-
-
-// Define port options
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
-// Set up Express app public folder
-app.use(express.static("public"));
+
+//setting static access to all the files in the public folder
+app.use(express.static("./public"));
 
 
-// Set up Express app to handle data parsing
+//MiddleWare to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 
-// Console log server start message
+//CRUD - GET /notes
+app.get("./notes.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.status(200);
+    console.log ("notes retrieved");
+});
+
+//CRUD - "*" aka index.html file
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+//CRUD - GET /api/notes
+app.get("./api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({message : `unable to retrieve notes`});
+        }
+        res.json(JSON.parse(data));
+    })
+});
+
+
+
+
+
+//console log the port number ti know servier is running
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`)
 });
